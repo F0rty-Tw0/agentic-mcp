d38dfeb85df1eb3f74484503fb66c2004965417c
Artiom Tofan <arto@queue-it.com>
2026-02-20 16:40:25 +0100

    chore(tests): 🗑️ remove unused test files and types
    This commit deletes obsolete test utility files and related types that are no longer in use, streamlining the codebase.

--- src/test-utils/vi-fn.types.ts
+++
@@ -1,3 +0,0 @@
-export type AsyncViFn<TArgs extends unknown[] = [], TResult = void> = (...args: TArgs) => Promise<TResult>;
-
-export type SyncViFn<TArgs extends unknown[] = [], TResult = void> = (...args: TArgs) => TResult;
