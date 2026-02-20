9c875d4aea05450caf48ba2c2f6acc90f5cf8b40
Artiom Tofan <arto@queue-it.com>
2026-02-20 16:40:59 +0100

    feat(constants): ✨ add command definition constants

---
+++ src/feature/ask/common/command-def.const.ts
@@ -0,0 +1,15 @@
+export const FLAG_MODEL = 'model';
+
+export const FLAG_WORKING_DIR = 'workingDir';
+
+export const FLAG_FILE = 'file';
+
+export const FLAG_AUTO_MODE = 'autoMode';
+
+export const FLAG_SANDBOX = 'sandbox';
+
+export const FLAG_EFFORT = 'effort';
+
+export const FLAG_MAX_BUDGET = 'maxBudget';
+
+export const FLAG_SYSTEM_PROMPT = 'systemPrompt';
