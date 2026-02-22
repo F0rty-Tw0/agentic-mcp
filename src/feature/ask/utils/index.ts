064989828cd666b466e362e89bc8df9584dc57fb
F0rty-Tw0 <artiomtofancv@gmail.com>
2026-02-22 02:47:08 +0100

    feat(stubs): ✨ add stubs for simple tools components
    - Create `index.ts` for exporting stubs related to simple tools.
    - This includes environment and execution result stubs.

---
+++ src/feature/ask/utils/index.ts
@@ -0,0 +1,1 @@
+export { getAskCommand, getFlag } from './command-def.util.ts';
---
+++ src/feature/simple-tools/common/stubs/index.ts
@@ -0,0 +1,11 @@
+export { SIMPLE_TOOLS_TEST_ENV_STUB } from './simple-tools-env.stub.ts';
+
+export {
+  SIMPLE_TOOLS_HELP_OUTPUT_RESULT_STUB,
+  SIMPLE_TOOLS_PING_VERSION_RESULT_STUB,
+  SIMPLE_TOOLS_SUCCESS_EXECUTION_RESULT_STUB,
+} from './simple-tools-execution-result.stub.ts';
+
+export { SIMPLE_TOOLS_PROVIDER_CONFIG_STUB } from './simple-tools-provider-config.stub.ts';
+
+export { SIMPLE_TOOLS_RESOLVED_PROVIDER_ENTRY_STUB } from './simple-tools-resolved-provider-entry.stub.ts';
