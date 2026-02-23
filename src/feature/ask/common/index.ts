4bd87d793f0dc7e4e9100544a47a3f5fa7fa266d
F0rty-Tw0 <artiomtofancv@gmail.com>
2026-02-23 02:11:57 +0100

    refactor(ask): ♻️ re-export new types from ask common index

--- src/feature/ask/common/index.ts
+++ src/feature/ask/common/index.ts
@@ -9,8 +9,34 @@ export {
   FLAG_WORKING_DIR,
 } from './command-def.const.ts';
 
-export type { ProgressContext } from '../../../shared/common/index.ts';
+export type { ProgressContext } from './progress-context.types.ts';
+
+export { ASK_STREAM_EVENT_SCHEMA } from './stream-event.types.ts';
+
+export type {
+  AskStreamChannel,
+  AskStreamDiagnostics,
+  AskStreamEvent,
+  AskStreamEventType,
+  AskStreamExecutionSummary,
+} from './stream-event.types.ts';
+
+export {
+  HEARTBEAT_IDLE_INTERVAL_MS,
+  MAX_STREAM_CHUNK_BYTES,
+  STREAM_COALESCE_WINDOW_MS,
+  STREAM_PROGRESS_START,
+  TERMINAL_EVENT_GRACE_TIMEOUT_MS,
+} from './streaming.const.ts';
 
 export type { AskToolArgs, BuiltArgs } from './tool-args.types.ts';
 
+export type { SessionMode } from './session-mode.type.ts';
+
+export type { AskJobRecord, AskJobState } from './ask-job.types.ts';
+
+export { ASK_JOB_TTL_MS, MAX_ASK_JOB_RECORDS } from './ask-job.const.ts';
+
 export { isLeveledFlag } from './tool-args.types.ts';
+
+export type { ProviderAttribution } from './attribution.types.ts';
