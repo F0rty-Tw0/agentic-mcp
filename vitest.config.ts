4952bfb802aa69e0d8db6a938f82e6245eabf5a1
Artiom Tofan <artiomtofancv@gmail.com>
2026-03-06 01:46:33 +0100

    chore(coverage): 🛠️ add coverage configuration for tests
    - Include coverage thresholds for lines, functions, branches, and statements.
    - Exclude specific files and directories from coverage reports.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,23 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['src/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/**/*.spec.ts',
+        'src/**/*.test.ts',
+        'src/**/index.ts',
+        'src/**/stubs/**',
+        'src/**/common/index.ts',
+        'src/types/**',
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 75,
+        statements: 80,
+      },
+    },
   },
 });
